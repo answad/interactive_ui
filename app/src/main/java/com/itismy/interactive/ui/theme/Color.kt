692ad644f3636e7c1c138cd0544c88f1ac3a04c9
answad <mymhj122@gmail.com>
2025-02-27 20:00:27 +0900

    Color

---
+++ app/src/main/java/com/itismy/interactive/ui/theme/Color.kt
@@ -0,0 +1,26 @@
+package com.itismy.interactive.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val transparentPurple = Color(0xFF9C27B0).copy(alpha = 0.7f)
+val transparentBlue = Color(0xFF2196F3).copy(alpha = 0.7f)
+val transparentLightBlue = Color(0xFF03A9F4).copy(alpha = 0.7f)
+val transparentGreen = Color(0xFF4CAF50).copy(alpha = 0.7f)
+val transparentRed = Color(0xFFF44336).copy(alpha = 0.6f)
+val transparentOrange = Color(0xFFFF9800).copy(alpha = 0.7f)
+val transparentYellow = Color(0xFFFFEB3B).copy(alpha = 0.6f)
+val transparentLightGreen = Color(0xFF8BC34A).copy(alpha = 0.8f)
+val transparentTeal = Color(0xFF009688).copy(alpha = 0.6f)
+val transparentIndigo = Color(0xFF3F51B5).copy(alpha = 0.6f)
+val transparentGrey = Color(0xFF9E9E9E).copy(alpha = 0.3f)
+val transparentBlack = Color(0xFF000000).copy(alpha = 0.2f)
+val transparentWhite = Color(0xFFFFFFFF).copy(alpha = 0.1f)
+
